2571929feb2c3afa91fbdffedcb3da4c612ec742
Carles Bruguera <carlesba@gmail.com>
2014-10-29 20:02:45 +0100

    Package skel

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '0.0'
+
+setup(name='flickup',
+      version=version,
+      description="Yet another Flickr File Uploader",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Carles Bruguera',
+      author_email='carlesba@gmail.com',
+      url='https://github.com/sunbit/flickup',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples']),
+      include_package_data=True,
+      test_suite="tests",
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          "python-flickr",
+          "rauth"
+      ],
+      extras_require={
+          'tests': []
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      flickup = flickup:main
+      """,
+      )
---
+++ tests/test_collections.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestCollections(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_add_collection(self):
+        self.assertEqual(True, True)
